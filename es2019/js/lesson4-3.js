a1867ef7ab31c67b931f02b06378056ff1c66c04
ultimater <cseve123@163.com>
2020-03-14 16:49:25 +0800

    es8 for of padStar async.await defineProperty getOwnPropertyDescriptor

---
+++ es2019/js/lesson4-3.js
@@ -0,0 +1,5 @@
+// 字符串补白
+// str.padStart(2, '0') str.padEnd()
+for (let i = 0; i < 32; i++) {
+  console.log(i.toString().padStart(2, '0'));
+}
---
+++ es2019/js/lesson4-4.js
@@ -0,0 +1,15 @@
+// 对象描述符 defineProperty
+const data = {
+  Por: '1/2',
+  Dub: '3/4',
+  Lim: '5/6'
+}
+
+Object.defineProperty(data, 'Lim', {
+  enumerable: false,
+  writable: false
+})
+console.log(Object.keys(data));
+// 查看数据权限
+console.log(Object.getOwnPropertyDescriptors(data));
+console.log(Object.getOwnPropertyDescriptor(data, 'Lim'));
